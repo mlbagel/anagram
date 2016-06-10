6827bd1ff2bcb20a05847c192c48a48f08160649
Michelle <mlbagell@gmail.com>
2016-06-10 11:46:51 -0400

    added new erb view

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,9 +1,22 @@
 get '/words' do
+  @page="home"
   @words = Word.all
   erb :"/words/index"
 end
 
+get '/words/new' do
+  @word = Word.new
+  @page="home"
+    erb :"/words/new"
+end
+
+post '/words' do
+    @word = Word.create(text: params[:text])
+    redirect "/words/#{@word.id}"
+end
+
 get '/words/:id' do
+  @page="answer"
   @word = Word.find(params[:id])
   erb :"/words/show"
 end
